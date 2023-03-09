9287c08f20fa72f80a945e7057332635fe62f90c
camchaney <cameron.chaney.vssa@gmail.com>
2023-03-08 22:40:35 -0800

    pmw3360 progress

--- mouse_sensing/PMW3360DM-polling/PMW3360DM-polling.ino
+++ mouse_sensing/PMW3360DM-polling/PMW3360DM-polling.ino
@@ -57,18 +57,21 @@
 #define Raw_Data_Burst  0x64
 #define LiftCutoff_Tune2  0x65
 
-//Set this to what pin your "INT0" hardware interrupt feature is on
-#define Motion_Interrupt_Pin 9
-
+// Pin Definitons
+#define Motion_Interrupt_Pin 2 //Set this to what pin your "INT0" hardware interrupt feature is on
 const int ncs = 10;  //This is the SPI "slave select" pin that the sensor is hooked up to
 
 byte initComplete=0;
-volatile int xydat[2];
+volatile int d_xy[2];
+volatile int xy[2] = {0,0};
 volatile byte movementflag=0;
 byte testctr=0;
 unsigned long currTime;
 unsigned long timer;
 unsigned long pollTimer;
+unsigned long dt = 20;
+
+int plotting = 1;
 
 //Be sure to add the SROM file into this sketch via "Sketch->Add File"
 extern const unsigned short firmware_length;
@@ -77,7 +80,7 @@ extern const unsigned char firmware_data[];
 void setup() {
   Serial.begin(9600);
   
-  pinMode (ncs, OUTPUT);
+  pinMode(ncs, OUTPUT);
   
   pinMode(Motion_Interrupt_Pin, INPUT);
   digitalWrite(Motion_Interrupt_Pin, HIGH);
@@ -91,181 +94,51 @@ void setup() {
 
   
   performStartup();  
-  
-  delay(5000);
-  
+
+  delay(500);
+  // delay(5000);
+
+  // Check initialization
   dispRegisters();
   initComplete=9;
 
 }
 
-void adns_com_begin(){
-  digitalWrite(ncs, LOW);
-}
-
-void adns_com_end(){
-  digitalWrite(ncs, HIGH);
-}
-
-byte adns_read_reg(byte reg_addr){
-  adns_com_begin();
-  
-  // send adress of the register, with MSBit = 0 to indicate it's a read
-  SPI.transfer(reg_addr & 0x7f );
-  delayMicroseconds(100); // tSRAD
-  // read data
-  byte data = SPI.transfer(0);
-  
-  delayMicroseconds(1); // tSCLK-NCS for read operation is 120ns
-  adns_com_end();
-  delayMicroseconds(19); //  tSRW/tSRR (=20us) minus tSCLK-NCS
-
-  return data;
-}
-
-void adns_write_reg(byte reg_addr, byte data){
-  adns_com_begin();
-  
-  //send adress of the register, with MSBit = 1 to indicate it's a write
-  SPI.transfer(reg_addr | 0x80 );
-  //sent data
-  SPI.transfer(data);
-  
-  delayMicroseconds(20); // tSCLK-NCS for write operation
-  adns_com_end();
-  delayMicroseconds(100); // tSWW/tSWR (=120us) minus tSCLK-NCS. Could be shortened, but is looks like a safe lower bound 
-}
-
-void adns_upload_firmware(){
-  // send the firmware to the chip, cf p.18 of the datasheet
-  Serial.println("Uploading firmware...");
-
-  //Write 0 to Rest_En bit of Config2 register to disable Rest mode.
-  adns_write_reg(Config2, 0x20);
-  
-  // write 0x1d in SROM_enable reg for initializing
-  adns_write_reg(SROM_Enable, 0x1d); 
-  
-  // wait for more than one frame period
-  delay(10); // assume that the frame rate is as low as 100fps... even if it should never be that low
-  
-  // write 0x18 to SROM_enable to start SROM download
-  adns_write_reg(SROM_Enable, 0x18); 
-  
-  // write the SROM file (=firmware data) 
-  adns_com_begin();
-  SPI.transfer(SROM_Load_Burst | 0x80); // write burst destination adress
-  delayMicroseconds(15);
-  
-  // send all bytes of the firmware
-  unsigned char c;
-  for(int i = 0; i < firmware_length; i++){ 
-    c = (unsigned char)pgm_read_byte(firmware_data + i);
-    SPI.transfer(c);
-    delayMicroseconds(15);
-  }
-
-  //Read the SROM_ID register to verify the ID before any other register reads or writes.
-  adns_read_reg(SROM_ID);
-
-  //Write 0x00 to Config2 register for wired mouse or 0x20 for wireless mouse design.
-  adns_write_reg(Config2, 0x00);
-
-  // set initial CPI resolution
-  adns_write_reg(Config1, 0x15);
-  
-  adns_com_end();
-  }
-
-
-void performStartup(void){
-  adns_com_end(); // ensure that the serial port is reset
-  adns_com_begin(); // ensure that the serial port is reset
-  adns_com_end(); // ensure that the serial port is reset
-  adns_write_reg(Power_Up_Reset, 0x5a); // force reset
-  delay(50); // wait for it to reboot
-  // read registers 0x02 to 0x06 (and discard the data)
-  adns_read_reg(Motion);
-  adns_read_reg(Delta_X_L);
-  adns_read_reg(Delta_X_H);
-  adns_read_reg(Delta_Y_L);
-  adns_read_reg(Delta_Y_H);
-  // upload the firmware
-  adns_upload_firmware();
-  delay(10);
-  Serial.println("Optical Chip Initialized");
-  }
-
-void UpdatePointer(void){
-  if(initComplete==9){
-
-    //write 0x01 to Motion register and read from it to freeze the motion values and make them available
-    adns_write_reg(Motion, 0x01);
-    adns_read_reg(Motion);
-
-    xydat[0] = (int)adns_read_reg(Delta_X_L);
-    xydat[1] = (int)adns_read_reg(Delta_Y_L);
-    
-    movementflag=1;
-    }
-  }
-
-void dispRegisters(void){
-  int oreg[7] = {
-    0x00,0x3F,0x2A,0x02  };
-  char* oregname[] = {
-    "Product_ID","Inverse_Product_ID","SROM_Version","Motion"  };
-  byte regres;
-
-  digitalWrite(ncs,LOW);
-
-  int rctr=0;
-  for(rctr=0; rctr<4; rctr++){
-    SPI.transfer(oreg[rctr]);
-    delay(1);
-    Serial.println("---");
-    Serial.println(oregname[rctr]);
-    Serial.println(oreg[rctr],HEX);
-    regres = SPI.transfer(0);
-    Serial.println(regres,BIN);  
-    Serial.println(regres,HEX);  
-    delay(1);
-  }
-  digitalWrite(ncs,HIGH);
-}
-
-
-int convTwosComp(int b){
-  //Convert from 2's complement
-  if(b & 0x80){
-    b = -1 * ((b ^ 0xff) + 1);
-    }
-  return b;
-  }
-  
-
 void loop() {
 
   currTime = millis();
-  
-  if(currTime > timer){    
-    Serial.println(testctr++);
+
+  if(currTime > timer){ 
+    // Timer   
+    // Serial.println(testctr++);
     timer = currTime + 2000;
-    }
+  }
     
   if(currTime > pollTimer){
+    // Get data
     UpdatePointer();
-    xydat[0] = convTwosComp(xydat[0]);
-    xydat[1] = convTwosComp(xydat[1]);
-      if(xydat[0] != 0 || xydat[1] != 0){
-        Serial.print("x = ");
-        Serial.print(xydat[0]);
-        Serial.print(" | ");
-        Serial.print("y = ");
-        Serial.println(xydat[1]);
-        }
-    pollTimer = currTime + 20;
-    }
-    
-  }
+    d_xy[0] = convTwosComp(d_xy[0]);
+    d_xy[1] = convTwosComp(d_xy[1]);
 
+    if(d_xy[0] != 0 || d_xy[1] != 0){
+      xy[0] = xy[0] + d_xy[0];     // update x measurements
+      xy[1] = xy[1] + d_xy[1];     // update x measurements
+
+      if(plotting) {
+        Serial.printf("dx:%i,dy:%i,",d_xy[0],d_xy[1]);
+        Serial.printf("x:%i,y:%i\n",xy[0],xy[1]);
+      } else {
+        Serial.printf("d_xy = (%i,",d_xy[0]);
+        Serial.printf(" %i)", d_xy[1]);
+        Serial.println();
+  
+        Serial.printf("xy = (%i,", xy[0]);
+        Serial.printf(" %i)", xy[1]);
+        Serial.println();
+        Serial.println();
+      }
+    }
+    pollTimer = currTime + dt;
+  }
+    
+}
