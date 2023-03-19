dc1f8f0d9419ee7c9dc947c1b09dce2959484e17
camchaney <cameron.chaney.vssa@gmail.com>
2023-03-19 15:28:10 -0700

    calibration done

--- mouse_sensing/basic_polling/basic_polling.ino
+++ mouse_sensing/basic_polling/basic_polling.ino
@@ -39,11 +39,25 @@ Module   Arduino
 
 #define SS  10   // Slave Select pin. Connect this to SS on the module.
 
+// Sensor properties
+int CPI = 2500;               // This value changes calibration coefficients
+float Cx[4] = {0.01003310926f,0.00996611521f,0.01008674602f,0.00997506234f};
+float Cy[4] = {0.01026588646f,0.01019056354f,0.01016570093f,0.01011531459f};
+
+int mmMode = 0;
 int x = 0;
 int y = 0;
+float xmm = 0;
+float ymm = 0;
 int dX = 0;
 int dY = 0;
+float dXmm = 0;
+float dYmm = 0;
 int plotting = 1;
+
+int firstPoint = 1;
+long timeLastPoll = 0;
+long timestepPoll = 100;       // microseconds (freq = 1,000,000/timestepPoll [Hz])
 
 PMW3360 sensor;
 
@@ -52,50 +66,71 @@ void setup() {
   while(!Serial);
   
   //sensor.begin(10, 1600); // to set CPI (Count per Inch), pass it as the second parameter
-  if(sensor.begin(SS))  // 10 is the pin connected to SS of the module.
-    Serial.println("Sensor initialization successed");
-  else
+  if(sensor.begin(SS, CPI)) {  // 10 is the pin connected to SS of the module.
+    Serial.print("Sensor initialization successed, with CPI = ");
+    Serial.println(sensor.getCPI());
+  }else{
     Serial.println("Sensor initialization failed");
+  }
+
+  //sensor.setCPI(12000);
+  //Serial.println(sensor.getCPI());
+  
   
   //sensor.setCPI(1600);    // or, you can set CPI later by calling setCPI();
 }
 
 void loop() {
-  PMW3360_DATA data = sensor.readBurst_simple();
-  
-  if(data.isOnSurface && data.isMotion)
-  {
-//    if(data.dx & 0x80) {
-//      data.dx = -1 * ((data.dx ^ 0xff) + 1);
-//    }
-//    if(data.dy & 0x80) {
-//      data.dy = -1 * ((data.dy ^ 0xff) + 1);
-//    }
-    dX = convTwosComp(data.dx);
-    dY = convTwosComp(data.dy);
-
-    x = x + dX;
-    y = y + dY;
+  if(micros() - timeLastPoll >= timestepPoll) {
+    timeLastPoll = micros();
+    // Collect sensor data
+    PMW3360_DATA data = sensor.readBurst_simple();
     
-    if(plotting) {
-      //Serial.printf("dx:%i,dy:%i,",dX,dY);
-      Serial.printf("dx:%i,dy:%i,",data.dx,data.dy);
-      Serial.printf("x:%i,y:%i",x,y);
-      Serial.println();
-    } else {
-      Serial.print(data.dx);
-      Serial.print("\t");
-      Serial.print(data.dy);
-      Serial.println();
+    if(data.isOnSurface && data.isMotion) {
+      // If there is movement...
+      dX = convTwosComp(data.dx);
+      dY = convTwosComp(data.dy);
+      if(mmMode) {
+        dXmm = dX*Cx[3];
+        dYmm = dY*Cy[3];
+      }
 
-//      Serial.printf("xy = (%i,", xy[0]);
-//      Serial.printf(" %i)", xy[1]);
-//      Serial.println();
-//      Serial.println();
+      if(firstPoint) {
+        firstPoint = 0;
+      }else{
+        if(mmMode) {
+          xmm = xmm + dXmm;
+          ymm = ymm + dYmm;
+        }else{
+          x = x + dX;
+          y = y + dY;
+        }
+      }
+      
+      if(plotting) {
+        if(mmMode) {
+          Serial.printf("dx:%f,dy:%f,",dXmm,dYmm);
+          //Serial.printf("dx:%i,dy:%i,",data.dx,data.dy);
+          Serial.printf("x:%f,y:%f",xmm,ymm);
+          Serial.println();
+        }else{
+          Serial.printf("dx:%i,dy:%i,",dX,dY);
+          //Serial.printf("dx:%i,dy:%i,",data.dx,data.dy);
+          Serial.printf("x:%i,y:%i",x,y);
+          Serial.println();
+        }
+      } else {
+        Serial.print(data.dx);
+        Serial.print("\t");
+        Serial.print(data.dy);
+        Serial.println();
+      }
+  //    Serial.print("dt=");
+  //    Serial.println(micros() - timeStart);
     }
   }
   
-  delay(10);
+  //delay(10);
 }
 
 
