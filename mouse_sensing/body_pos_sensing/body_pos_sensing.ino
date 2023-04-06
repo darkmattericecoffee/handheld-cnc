78ab059039397e3f74e15fc12d938454ee8dfcd4
camchaney <cameron.chaney.vssa@gmail.com>
2023-04-05 18:32:51 -0700

    getting rigid body position and orientation

--- mouse_sensing/body_pos_sensing/body_pos_sensing.ino
+++ mouse_sensing/body_pos_sensing/body_pos_sensing.ino
@@ -7,10 +7,10 @@ Sensor configuration:
     NOTE: old config didn't obey the right hand rule. We are now flipping x.
 */
 
-#define SS1   10   // Slave Select pin. Connect this to SS on the module.
-#define SS2   39
+#define SS0   41   // Chip select pin. Connect this to SS on the module.
+#define SS1   39
+#define SS2   10
 #define SS3   40
-#define SS0   41
 
 
 // Sensor properties
@@ -27,8 +27,8 @@ const float yOff[4] = {l/2,l/2,-l/2,-l/2};
 int plotting = 1;
 
 int firstPoint = 1;
-long timeLastPoll = 0;
-long dt = 100;       // microseconds (freq = 1,000,000/timestepPoll [Hz])
+long unsigned timeLastPoll = 0;
+long unsigned dt = 500;       // microseconds (freq = 1,000,000/timestepPoll [Hz])
 
 // Measured quantities
 int dX[4] = {0,0,0,0};
@@ -45,8 +45,8 @@ float estVelX[4] = {0.0f,0.0f,0.0f,0.0f};
 float estVelY[4] = {0.0f,0.0f,0.0f,0.0f};
 float estVelX1 = 0.0f;
 float estVelY1 = 0.0f;
-float estPosX = 0.0f;
-float estPosY = 0.0f;
+float estPosX = 0.0f;                         // router center position - x
+float estPosY = 0.0f;                         // router center position - y
 float estAngVel[8] = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
 float estAngVel1 = 0.0f;
 float estYaw = 0.0f;                          // Angle estimation
@@ -96,7 +96,7 @@ void setup() {
   for (int i = 0; i<4; i++) {
     xmm[i] = xmm[i] + xOff[i];
     ymm[i] = ymm[i] + yOff[i];
-    Serial.printf("%i(%f,%f),",i+1,xmm[i],ymm[i]);
+    Serial.printf("%i(%.2f,%.2f),",i+1,xmm[i],ymm[i]);
   }
   Serial.println();
   
@@ -105,6 +105,7 @@ void setup() {
 
 void loop() {
   if(micros() - timeLastPoll >= dt) {
+    //Serial.println(micros() - timeLastPoll);
     timeLastPoll = micros();
 
     // Sensing ---------------------------------------------------------------------
@@ -113,19 +114,21 @@ void loop() {
     PMW3360_DATA data1 = sensor1.readBurst_simple();
     PMW3360_DATA data2 = sensor2.readBurst_simple();
     PMW3360_DATA data3 = sensor3.readBurst_simple();
-    
-    if(data1.isOnSurface && data1.isMotion) {   // If movement...
-      // Only checking sensor 1 rn (TODO: check all of them and account for misreads)
+
+    int yup = 1;
+    if(yup) {
+    //if(data1.isOnSurface && data1.isMotion) {   // If movement...
+      // **Only checking sensor 1 rn (TODO: check all of them and account for misreads)**
 
       // Sensor velocity sensing
-      dXmm[0] = convTwosComp(data0.dx)*Cx[0];       // maybe divide by dt (would be slower, but more robust?)
-      dXmm[1] = convTwosComp(data1.dx)*Cx[1];
-      dXmm[2] = convTwosComp(data2.dx)*Cx[2];
-      dXmm[3] = convTwosComp(data3.dx)*Cx[3];
-      dYmm[0] = convTwosComp(data0.dy)*Cx[0];
-      dYmm[1] = convTwosComp(data1.dy)*Cx[1];
-      dYmm[2] = convTwosComp(data2.dy)*Cx[2];
-      dYmm[3] = convTwosComp(data3.dy)*Cx[3];
+      dXmm[0] = -convTwosComp(data0.dx)*Cx[0] / dt;       // maybe divide by dt (would be slower, but more robust?)
+      dXmm[1] = -convTwosComp(data1.dx)*Cx[1] / dt;
+      dXmm[2] = -convTwosComp(data2.dx)*Cx[2] / dt;
+      dXmm[3] = -convTwosComp(data3.dx)*Cx[3] / dt;
+      dYmm[0] = convTwosComp(data0.dy)*Cx[0] / dt;
+      dYmm[1] = convTwosComp(data1.dy)*Cx[1] / dt;
+      dYmm[2] = convTwosComp(data2.dy)*Cx[2] / dt;
+      dYmm[3] = convTwosComp(data3.dy)*Cx[3] / dt;
 
 //      dXmm = dX*Cx[3];
 //      dYmm = dY*Cy[3];
@@ -154,19 +157,21 @@ void loop() {
       for (int i = 0; i<8; i++) {
         // Simple average of angular velocities
         sumAngVel = sumAngVel + estAngVel[i];
+        //Serial.printf("w%i:%f,",i,estAngVel[i]);
       }
       estAngVel1 = sumAngVel / 8.0f;
-      estYaw = estYaw + estAngVel1;
+      estYaw = estYaw + estAngVel1*dt;
+      //Serial.printf("w:%f",estAngVel1);
 
       // Body position estimation
-      estVelX[0] =  dXmm[0]*cosf(estYaw) - dYmm[0]*sinf(estYaw) + 0.5*estAngVel*l*(-sinf(estYaw)+cosf(estYaw));
-      estVelX[1] =  dXmm[1]*cosf(estYaw) - dYmm[1]*sinf(estYaw) + 0.5*estAngVel*l*(sinf(estYaw)+cosf(estYaw));
-      estVelX[2] =  dXmm[2]*cosf(estYaw) - dYmm[2]*sinf(estYaw) + 0.5*estAngVel*l*(-sinf(estYaw)-cosf(estYaw));
-      estVelX[3] =  dXmm[3]*cosf(estYaw) - dYmm[3]*sinf(estYaw) + 0.5*estAngVel*l*(sinf(estYaw)-cosf(estYaw));
-      estVelY[0] =  dXmm[0]*sinf(estYaw) + dYmm[0]*cosf(estYaw) + 0.5*estAngVel*l*(cosf(estYaw)+sinf(estYaw));
-      estVelY[1] =  dXmm[1]*sinf(estYaw) + dYmm[1]*cosf(estYaw) + 0.5*estAngVel*l*(-cosf(estYaw)+sinf(estYaw));
-      estVelY[2] =  dXmm[2]*sinf(estYaw) + dYmm[2]*cosf(estYaw) + 0.5*estAngVel*l*(cosf(estYaw)-sinf(estYaw));
-      estVelY[3] =  dXmm[3]*sinf(estYaw) + dYmm[3]*cosf(estYaw) + 0.5*estAngVel*l*(-cosf(estYaw)-sinf(estYaw));
+      estVelX[0] = dXmm[0]*cosf(estYaw)-dYmm[0]*sinf(estYaw) + 0.5*estAngVel1*l*(-sinf(estYaw)+cosf(estYaw));
+      estVelX[1] = dXmm[1]*cosf(estYaw)-dYmm[1]*sinf(estYaw) + 0.5*estAngVel1*l*(sinf(estYaw)+cosf(estYaw));
+      estVelX[2] = dXmm[2]*cosf(estYaw)-dYmm[2]*sinf(estYaw) + 0.5*estAngVel1*l*(-sinf(estYaw)-cosf(estYaw));
+      estVelX[3] = dXmm[3]*cosf(estYaw)-dYmm[3]*sinf(estYaw) + 0.5*estAngVel1*l*(sinf(estYaw)-cosf(estYaw));
+      estVelY[0] = dXmm[0]*sinf(estYaw)+dYmm[0]*cosf(estYaw) + 0.5*estAngVel1*l*(cosf(estYaw)+sinf(estYaw));
+      estVelY[1] = dXmm[1]*sinf(estYaw)+dYmm[1]*cosf(estYaw) + 0.5*estAngVel1*l*(-cosf(estYaw)+sinf(estYaw));
+      estVelY[2] = dXmm[2]*sinf(estYaw)+dYmm[2]*cosf(estYaw) + 0.5*estAngVel1*l*(cosf(estYaw)-sinf(estYaw));
+      estVelY[3] = dXmm[3]*sinf(estYaw)+dYmm[3]*cosf(estYaw) + 0.5*estAngVel1*l*(-cosf(estYaw)-sinf(estYaw));
       float sumVelX = 0.0f;
       float sumVelY = 0.0f;
       for (int i = 0; i<4; i++) {
@@ -174,15 +179,18 @@ void loop() {
         sumVelX = sumVelX + estVelX[i];
         sumVelY = sumVelY + estVelY[i];
       }
+      
       estVelX1 = sumVelX / 4.0f;
       estVelY1 = sumVelY / 4.0f;
-      estPosX = estPosX + estVelX1;
-      estPosY = estPosY + estVelY1;
+      estPosX = estPosX + estVelX1*dt;
+      estPosY = estPosY + estVelY1*dt;
       
-      //Serial.printf("dx:%f,dy:%f,",dXmm,dYmm);
-      //Serial.printf("dx:%i,dy:%i,",data.dx,data.dy);
-      //Serial.printf("x:%f,y:%f",estPosX,estPosY);
-      Serial.printf("x:%f,y:%f",xmm[1],ymm[1]);
+      //Serial.printf("dx:%f,dy:%f",dXmm,dYmm);
+      //Serial.printf("dx:%i,dy:%i",data.dx,data.dy);
+      Serial.printf("x:%f,y:%f,theta:%f",estPosX,estPosY,estYaw);
+//      Serial.printf("w1:%f,w2:%f,w3:%f,w4:%f,w5:%f,w6:%f,w7:%f,w8:%f",estAngVel[0],estAngVel[1],
+//        estAngVel[2],estAngVel[3],estAngVel[4],estAngVel[5],estAngVel[6],estAngVel[7]);
+      //Serial.printf("x:%f,y:%f",xmm[1],ymm[1]);
       Serial.println();
     }
   }
