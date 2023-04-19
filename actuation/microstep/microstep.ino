aa1bccee9549232ce0dc4ec1c94d6c3728b24241
JobanSM <98131707+JobanSM@users.noreply.github.com>
2023-04-19 12:27:56 -0700

    jost recent changes

--- actuation/microstep/microstep.ino
+++ actuation/microstep/microstep.ino
@@ -6,49 +6,49 @@ const int stepsPerRevolution = 200;
 const int MS1 = 21;
 const int MS2 = 14;
 
-//void iterate(int arr[], int size){
-//  delay(2000);
-//  for (int i = 0; i<size; i++){
-//    Serial.println(String(arr[i]));
-//    if (arr[i]<0) {
-//      digitalWrite(dirPin, HIGH);
-//    }
-//    else {
-//      digitalWrite(dirPin, LOW);
-//    }
-//    double turns = abs(arr[i]/0.8);
-//    for(int x = 0; x < turns*stepsPerRevolution; x++)
-//    {
-//      digitalWrite(stepPin, HIGH);
-//      delayMicroseconds(2000);
-//      digitalWrite(stepPin, LOW);
-//      delayMicroseconds(2000);
-//    }
-//    delay(2000);
-//  }
-//}
+void iterate(int arr[], int size){
+  delay(2000);
+  for (int i = 0; i<size; i++){
+    Serial.println(String(arr[i]));
+    if (arr[i]<0) {
+      digitalWrite(dirPin, HIGH);
+    }
+    else {
+      digitalWrite(dirPin, LOW);
+    }
+ 
+    for(int x = 0; x < (12800/.8)*abs(arr[i]); x++)
+    {
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(20);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(20);
+    }
+    delay(2000);
+  }
+}
 void setup()
 {
   // Declare pins as Outputs
   Serial.begin(115200);
-  //pinMode(MS1, OUTPUT);
-  //digitalWrite(MS1, HIGH);
-  //pinMode(MS2, OUTPUT);
-  //digitalWrite(MS2, HIGH);
+  pinMode(MS1, OUTPUT);
+  digitalWrite(MS1, LOW);
+  pinMode(MS2, OUTPUT);
+  digitalWrite(MS2, HIGH);
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-  //int myArray[] = {5,-5,5,-5};
+  int myArray[] = {5,-5,5,-5};
   
 digitalWrite(dirPin,HIGH);
   int arraySize = 4;
- // iterate(myArray,arraySize);
+ iterate(myArray,arraySize);
 
 }
 void loop()
 {
    digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2000);
+      delayMicroseconds(20);
       digitalWrite(stepPin, LOW);
-      delayMicroseconds(2000);
+      delayMicroseconds(20);
   
 }
