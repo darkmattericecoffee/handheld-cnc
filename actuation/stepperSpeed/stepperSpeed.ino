1c85bdc4aa3776e15721c2d14f9dbe3283f886e3
JobanSM <98131707+JobanSM@users.noreply.github.com>
2023-04-19 12:30:42 -0700

    stepper speed

--- actuation/stepperSpeed/stepperSpeed.ino
+++ actuation/stepperSpeed/stepperSpeed.ino
@@ -17,10 +17,11 @@
 #define DRIVER_ADDRESS     0b00       // TMC2209 Driver address according to MS1 and MS2
 #define R_SENSE            0.11f 
 int count = 0;
-int Conv = 250*64;
-int myArray[] = {5,-2,2,-5};
-int vel[] = {2,1,3,4};
+int Conv = 250*8;
+int myArray[] = {2,-2,2,-20};
+int vel[] = {1,-2,1,-5};
 int size = 4;
+bool began = false;
 // Define motor interface type
 #define motorInterfaceType 1
 
@@ -33,9 +34,9 @@ void setup() {
   pinMode(MS1, OUTPUT);
   digitalWrite(MS1, LOW);
   pinMode(MS2, OUTPUT);
-  digitalWrite(MS2, HIGH);
-  myStepper.setMaxSpeed(12800*2);
-  myStepper.setAcceleration(100*16*3*10);
+  digitalWrite(MS2, LOW);
+  myStepper.setMaxSpeed(12800*20);
+  myStepper.setAcceleration(100*16*2*10);
  // myStepper.setSpeed(200);
   //myStepper.moveTo(200);
   myStepper.setCurrentPosition(0);
@@ -45,11 +46,21 @@ void setup() {
 }
 
 void loop() {
-  myStepper.setSpeed(vel[count]*Conv);
-  myStepper.moveTo(Conv*myArray[count]);
-  myStepper.runToPosition();
-  count = count + 1;
-  if(count == size - 1) {
-    count = 0;
+  if (began == false) {
+    began = true;
+    myStepper.moveTo(Conv*myArray[count]);
+    myStepper.setMaxSpeed(vel[count]*Conv);
+  } else {
+    if (myStepper.distanceToGo() != 0) {
+      //delay(100);
+      myStepper.run();
+    } else {
+      began = false;
+      count = count + 1;
+      delay(1000);
+      if(count == size) {
+        count = 0;
+      }
+    }
   }
 }
