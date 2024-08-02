d6f5d4d82917ee01e8f21e56f1b0d745aecdafc2
Matt Morrison <mcharlesmorrison@berkeley.edu>
2024-08-02 16:46:26 -0700

    added segment categorization, in function for given gantry orientation, we have a true or false whether we can cut that point

--- dev/gCode/gCode_path_parser.py
+++ dev/gCode/gCode_path_parser.py
@@ -1,10 +1,10 @@
 import numpy as np
+import pandas as pd
 
 
-def read_gcode(file_path):
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-    return lines
+def read_gcode_csv(file_path):
+    df = pd.read_csv(file_path)
+    return df
 
 
 def line_from_point_point(pt1, pt2):
@@ -17,19 +17,27 @@ def line_from_point_point(pt1, pt2):
     return [m, b]
 
 
-def create_semgents(gcode_df, gantry_orientation):
+def create_segments(gcode_df, gantry_orientation):
     # compute direction between point i and point i+1
     # see if it is within 45 deg of gantry orientation
     # label green or red, continue
+    gcode_df["can_cut"] = False
     for i in range(len(gcode_df) - 1):
+        print("pt1 = " + str([gcode_df.x[i], gcode_df.y[i]]))
+        print("pt2 = " + str([gcode_df.x[i + 1], gcode_df.y[i + 1]]))
         mi, bi = line_from_point_point(
             [gcode_df.x[i], gcode_df.y[i]], [gcode_df.x[i + 1], gcode_df.y[i + 1]]
         )
-        segment_angle = 90 - np.rad2deg(np.arctan(mi))
+        if np.isnan(mi):
+            segment_angle = 0
+        else:
+            segment_angle = 90 - np.rad2deg(np.arctan(mi))
         # TODO: need to make sure we stay on one side of this angle for a continuous segment
         # ie we cannot go from 30 to -30 or somebting even though it's in range.....
         if abs(segment_angle) - gantry_orientation < 45:
-            print("GREEEEEEEN")
+            gcode_df.loc[i, "can_cut"] = True
+
+    return gcode_df
 
 
 # take in file
@@ -39,8 +47,9 @@ def create_semgents(gcode_df, gantry_orientation):
 
 
 def main():
-    my_lines = read_gcode("dev/gCode/test1/generic_test02.nc")
-    print(my_lines)
+    my_df = read_gcode_csv("dev/gCode/basePlate_test.csv")
+    gcode_df_with_segment_flag = create_segments(my_df, 0)
+    print("hi")
 
 
 if __name__ == "__main__":
